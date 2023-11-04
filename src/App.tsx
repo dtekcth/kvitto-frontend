c8171a7b22fa7aa8827b2b6a6401fd4b36e9cdd7
viloz1 <simonjohansson312@gmail.com>
2023-11-04 14:05:20 +0100

    made ts better

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,10 @@
 import logo from './logo.svg';
 import './App.css';
+import { MyButton } from './design-system/button/button';
+import React from 'react';
 
 function App() {
+  var l: string = ""
   return (
     <div className="App">
       <header className="App-header">
@@ -15,6 +18,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
+          <MyButton></MyButton>
           Learn React
         </a>
       </header>
