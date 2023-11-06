c1e91f90c4b4fcd9e38fee37bcc35683da396dcd
viloz1 <simonjohansson312@gmail.com>
2023-11-06 13:35:52 +0100

    fix lint

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
-import logo from './logo.svg';
+import { ReactComponent as ReactLogo } from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <ReactLogo className="App-logo"/>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn Reac
         </a>
       </header>
     </div>
